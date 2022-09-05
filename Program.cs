39451a75a07b9be18261c9bd1cb3978e1e047ed2
KordeEE <korde@inbox.ru>
2022-09-05 12:50:50 +0200

    Fill new massiv

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, длина который меньше либо равна 3 символа. 
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine("");
+}
+
+string[] stringArrayA = new string[5];
+
+for (int i = 0; i < stringArrayA.Length; i++)
+{
+    Console.Write("Input syting element inn array: ");
+    string str = Convert.ToString(Console.ReadLine());
+    stringArrayA[i] = str;
+}
+
+ShowArray(stringArrayA);
