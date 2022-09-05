83c65a2f6c226c6a31526e857b751454866a5c61
KordeEE <korde@inbox.ru>
2022-09-05 14:10:21 +0200

    Final

--- Program.cs
+++ Program.cs
@@ -8,13 +8,48 @@ void ShowArray(string[] array)
     Console.WriteLine("");
 }
 
-string[] stringArrayA = new string[5];
+string[] MyNewArray(string[] array)
+{
+    int m = 0;
+    string[] stringArrayB = new string[5];
+
+    for(int j = 0; j < array.Count(); j++)
+    {
+        if(array[j].Length <= 3)
+        {   
+            stringArrayB[m] = array[j];
+            m++;
+        }
+    }
+    return stringArrayB;
+}
+
+void CheckArray(string[] array)
+{
+    int count = 0;
+    for(int j = 0; j < array.Count(); j++)
+    {   
+        if(array[j].Length <= 3) count++;
+    }
+    if(count == 0)
+    {
+        Console.Write("В заданном массиве нет элементов длина которых меньше 3х символов");
+    }
+}
+
+int size = 5;
+string[] stringArrayA = new string[size];
 
 for (int i = 0; i < stringArrayA.Length; i++)
 {
-    Console.Write("Input syting element inn array: ");
+    Console.Write("Input element in array: ");
     string str = Convert.ToString(Console.ReadLine());
     stringArrayA[i] = str;
 }
 
 ShowArray(stringArrayA);
+//CheckArray(stringArrayA);
+
+string[] MyNewStringArray = MyNewArray(stringArrayA);
+
+ShowArray(MyNewStringArray);
